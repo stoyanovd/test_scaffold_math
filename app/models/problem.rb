830259aabd65448f2cd644e5d319561737903311
dima <stoyanovd@mail.ru>
2014-12-17 00:57:43 +0300

    Small fix about not existing image.

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,8 +1,6 @@
 class Problem < ActiveRecord::Base
   validates :number, numericality: true,
             presence: true
-  validates :body, presence: true,
-            length: {minimum: 1}
 
 
   belongs_to :problem_list
@@ -11,14 +9,20 @@ class Problem < ActiveRecord::Base
 
   validate :must_exists
 
-  has_attached_file :figure, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :default_url => "/images/:style/figure.png"
+  has_attached_file :figure, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :default_url => nil
 
   validates_attachment_content_type :figure, :content_type => /\Aimage\/.*\Z/
 
 
   private
   def must_exists
-    errors.add(:problem_list_id, 'must exists') unless ProblemList.exists?(id: self.problem_list)
+    unless ProblemList.exists?(id: self.problem_list)
+      errors.add(:problem_list_id, 'must exists')
+      return
+    end
     errors.add(:problem_list_id, 'can have not more than four problems') if self.problem_list.problems.count >= 5
+    if (self.body == nil || self.body.length == 0) && !(self.figure.file?)
+      errors.add(:body, 'or image must exists')
+    end
   end
 end

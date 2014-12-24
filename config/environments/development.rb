67f2463778b40d926a07b6000305d786f5537611
dima <stoyanovd@mail.ru>
2014-12-24 04:49:49 +0300

    Temporary add compressors to development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,13 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  #  config.assets.debug = true
+
+  # Temporary way to show short assets
   config.assets.debug = false
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
+  
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.

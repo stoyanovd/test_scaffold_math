af2b0fdda57bb8ee2c8c3a5ef3c95b5dd4f7172f
dima <stoyanovd@mail.ru>
2014-12-24 05:51:08 +0300

    Delete excess precompilation.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,6 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   config.serve_static_assets = true
-  config.assets.precompile += %w( *.css *.js )
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
